a5debb99089c8119df200022b1b4642b5e2b1949
arashesmailian <arashesmailian@gmail.com>
2021-11-03 20:18:22 +0330

    hello

---
+++ Q2.js
@@ -0,0 +1,14 @@
+function mapingOnAge(input){
+    return {
+        input.
+    }
+}
+
+let test = [{name : 'arash1', age: 22},
+    {name : 'arash2', age: 23},
+    {name : 'arash3', age: 21},
+    {name : 'arash4', age: 22},
+    {name : 'arash5', age: 23},
+    {name : 'arash6', age: 20}
+]
+console.log(mapingOnAge(test))
